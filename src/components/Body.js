21a34875f698ef32fbc9aef00650bcbddef69bfa
Ashrayaa <mailtoashrayaa@gmail.com>
2023-01-18 00:03:17 +0530

    ui fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1798,7 +1798,7 @@ const RestaurantCard = ({
         </h4>
 
         <div
-          className="flex text-[#202020] font-medium gap-32
+          className="flex text-[#202020] font-medium gap-24
          items-center "
         >
           <div className="flex gap-1 items-center">
