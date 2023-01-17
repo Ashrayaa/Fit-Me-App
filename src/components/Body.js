3197a15f59fee01914400bc4e508568acf3b20f4
Ashrayaa <mailtoashrayaa@gmail.com>
2023-01-17 23:54:25 +0530

    ui fixed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1798,7 +1798,7 @@ const RestaurantCard = ({
         </h4>
 
         <div
-          className="flex text-[#202020] font-medium gap-12
+          className="flex text-[#202020] font-medium gap-32
          items-center "
         >
           <div className="flex gap-1 items-center">
@@ -1808,7 +1808,7 @@ const RestaurantCard = ({
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              className="w-6 h-6 text-[#FC8019]"
+              className="w-6 h-5 text-[#FC8019]"
             >
               <path
                 stroke-linecap="round"
@@ -1829,7 +1829,22 @@ const RestaurantCard = ({
             <h4 className="text-sm ">{avgRating}</h4>
           </div>
         </div>
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-1 items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-5 text-[#FC8019]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+            />
+          </svg>
+
           <h4 className="text-sm font-medium">{lastMileTravelString}</h4>
         </div>
       </div>
@@ -1839,7 +1854,7 @@ const RestaurantCard = ({
 
 const Body = () => {
   return (
-    <div className="grid grid-flow-row grid-cols-4 gap-y-8 mr-12">
+    <div className="grid grid-flow-row grid-cols-3 gap-y-8 mr-12 px-16">
       {restaurantlist.map((restaurant) => {
         return <RestaurantCard {...restaurant.data} />;
       })}
