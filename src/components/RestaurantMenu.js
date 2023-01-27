1c5d4fc722f706645e2ba1446c65bdc56862c3cd
Ashrayaa <mailtoashrayaa@gmail.com>
2023-01-27 10:48:19 +0530

    ui fix

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -27,7 +27,7 @@ const RestaurantMenu = () => {
   // if(!restaurant){
   //   return <Shimmer/>;
   // }
-
+  // let uniqueCategories = [...new Set(restaurant?.category)];
   return !restaurant ? (
     <Shimmer />
   ) : (
@@ -37,14 +37,14 @@ const RestaurantMenu = () => {
       <div className="flex flex-col justify-center gap-6 lg:flex-row bg-black text-white p-12 lg:justify-around">
         {/* image */}
         <img
-          className=" w-72 rounded-md"
+          className=" w-72 rounded-md lg:ml-24"
           src={IMG_CDN_URL + restaurant?.cloudinaryImageId}
           key="res1"
           id="res12"
           alt="KFC"
         />
         {/* div for restarant */}
-        <div className="flex flex-col gap-2 justify-center">
+        <div className="flex flex-col gap-2 justify-center lg:mr-24">
           <h1 className="text-2xl font-medium">{restaurant?.name}</h1>
 
           <h4>{restaurant?.cuisines?.join(" , ")}</h4>
@@ -69,12 +69,12 @@ const RestaurantMenu = () => {
                 {restaurant?.totalRatingsString}
               </h4>
             </div>
-            <div className="bg-white h-12 text-white border-2 border-solid"></div>
+            <div className="bg-white w-px text-white"></div>
             <div className="flex flex-col gap-2">
               <h4 className="text-sm font-medium">29 Mins</h4>
               <h4 className="text-xs font-light">Delivery Time</h4>
             </div>
-            <div className="bg-white h-12 text-white border-2 border-solid"></div>
+            <div className="bg-white h-12 w-px text-white"></div>
 
             <div className="flex flex-col gap-2">
               <h4 className="text-sm font-medium">
@@ -85,7 +85,7 @@ const RestaurantMenu = () => {
           </div>
         </div>
         {/* div for offers */}
-        <div className="border-2 rounded-xl flex flex-col gap-4 border-dashed border-[#FC8019] p-6">
+        <div className="border-2 rounded-xl flex flex-col gap-4 border-dashed border-[#FC8019] p-6 mr-32">
           <h1 className="text-[#FC8019] font-medium text-lg">Offers</h1>
           <div className="flex gap-4">
             <img src={Offertag} key="offertag" className="w-6" />
@@ -104,9 +104,9 @@ const RestaurantMenu = () => {
       {/* search bar */}
       {/* favourite */}
 
-      <div className="grid grid-flow-row grid-cols-4 p-12">
-        <div className="hidden lg:flex flex-col gap-4 mr-4">
-          {Object.values(restaurant?.menu?.items).map((item) => (
+      <div className="grid grid-flow-row grid-cols-4 p-12 px-24">
+        <div className="hidden lg:flex flex-col gap-4 mr-6">
+          {Object.values(restaurant?.menu?.items).map((item,index) => (
             <h6
               className="font-medium text-sm hover:text-[#FC8019] text-right"
               id={item.category}
@@ -115,7 +115,9 @@ const RestaurantMenu = () => {
             </h6>
           ))}
         </div>
-        <div className="bg-[#808080] w-0.5 border-2 border-solid"></div>
+        <div className="w-px bg-gray-200 border-0 dark:bg-gray-700"></div>
+
+        {/* <div className="bg-[#808080] w-0.5 border-2 border-solid"></div> */}
 
         {/* menu list */}
         <div className="-ml-72">
@@ -138,7 +140,7 @@ const RestaurantMenu = () => {
                     <h4 className="text-[#000000] text-sm font-medium">
                       ₹{item?.price}
                     </h4>
-                    <h5 className="text-[#808080] text-xs font-light mt-4 w-[500px]">
+                    <h5 className="text-[#808080] text-sm font-light mt-4 w-[430px]">
                       {item?.description}
                     </h5>
                   </div>
@@ -152,12 +154,12 @@ const RestaurantMenu = () => {
                       }
                     />
                     <button className="text-[#1AC84B] w-24 -mt-5 font-medium bg-white px-6 py-2 border rounded-lg shadow-lg border-white z-10">
-                      ADD
+                      Add
                     </button>
                   </div>
                 </div>
-
-                <div className="bg-[#808080] mt-8 mb-12 h-0.5 border-2 border-solid"></div>
+                <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+                {/* <div className="bg-[#808080] mt-8 mb-12 h-0.5 border-2 border-solid"></div> */}
               </div>
               // (
               //   <div>
