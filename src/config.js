eaf4c379499d86f60a9a4e41507d080b76bd6b4e
Ashrayaa <mailtoashrayaa@gmail.com>
2023-02-25 11:13:37 +0530

    cors issue fix

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export const IMG_CDN_URL =
 export const SWIGGY_API_URL =
   "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&page_type=DESKTOP_WEB_LISTING";
 
-export const FETCH_MENU_URL = "https://www.swiggy.com/dapi/menu/v4/full?lat=8.5241391&lng=76.9366376&menuId=";
+export const FETCH_MENU_URL = "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/v4/full?lat=8.5241391&lng=76.9366376&menuId=";
