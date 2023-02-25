4f456112bdf7738ab1255df848da561b8add3675
Ashrayaa <mailtoashrayaa@gmail.com>
2023-02-25 11:11:04 +0530

    cors issue fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="flex flex-col p-6 gap-12 sm:gap-16 lg:ml-[35%] lg:p-10 lg:border lg:border-neutral-100 lg:w-[500px] lg:py-12 lg:shadow-lg lg:rounded-lg justify-center items-center align-middle">
       <div className="grid sm:grid-cols-2 justify-center sm:gap-20 lg:gap-24 items-center grid-flow-row">
         <div className="flex flex-col justify-center items-center gap-3">
           <h1 className="text-5xl font-medium">Login</h1>
           <h5 className="text-sm font-medium">
-            or <span className="text-[#FC8019] cursor-pointer">
-              <Link to="/signup">create an account</Link></span>
+            or{" "}
+            <span className="text-[#FC8019] cursor-pointer">
+              <Link to="/signup">create an account</Link>
+            </span>
           </h5>
         </div>
         <img
@@ -18,21 +26,49 @@ const Login = () => {
         />
       </div>
 
-      <div className="flex flex-col gap-4 lg:w-96">
-      <input
+      <form
+        className="flex flex-col gap-4 lg:w-96"
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <input
+          {...register("email", {
+            required: "Your email is required",
+            pattern: /^\S+@\S+$/i,
+          })}
           className="border p-4 border-gray-100 shadow-sm rounded-md outline-none"
           type="email"
           id="email"
           name="email"
           placeholder="Email"
         />
+        <p>
+          {errors.email && (
+            <span className="text-xs font-medium text-red-500">
+              Please enter a valid email address
+            </span>
+          )}
+        </p>
         <input
+          {...register("password", {
+            required: "Password is a required field",
+            minlength: 8,
+          })}
           className="border p-4 border-gray-100 shadow-sm rounded-md outline-none"
           type="password"
           id="password"
           name="password"
           placeholder="Password"
         />
+        <p>
+          {errors.password && (
+            <span className="text-xs font-medium text-red-500">
+              Please enter password with minimum 8 characters
+            </span>
+          )}
+        </p>
+
         {/* <input
           className="border p-4 border-gray-100 shadow-sm rounded-md outline-none"
           placeholder="Phone Number"
@@ -47,7 +83,7 @@ const Login = () => {
         <p className="font-normal text-[#808080] text-xs text-center">
           By clicking on Login, I accept the Terms & Conditions & Privacy Policy
         </p>
-      </div>
+      </form>
     </div>
   );
 };
--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const SWIGGY_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&page_type=DESKTOP_WEB_LISTING";
+  "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&page_type=DESKTOP_WEB_LISTING";
 
 export const FETCH_MENU_URL = "https://www.swiggy.com/dapi/menu/v4/full?lat=8.5241391&lng=76.9366376&menuId=";
